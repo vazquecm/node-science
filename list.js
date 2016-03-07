60d398555ad8fd54be023129e5170a54b590228e
Carmen Vazquez-Marin <vazquecm@gmail.com>
2016-03-07 17:12:20 -0600

    sacrifice memory for speed in list

--- list.js
+++ list.js
@@ -10,18 +10,16 @@ class Node {
 class List {
   constructor () {
     this.head = null
+    this.tail = null
   }
 
   add (node) {
     if (this.head) {
-      let current = this.head
-
-      while (current.next) {
-        current = current.next
-      }
-      current.next = node
+      this.tail.next = node
+      this.tail = node
     } else {
       this.head = node
+      this.tail = node
     }
   }
 }
